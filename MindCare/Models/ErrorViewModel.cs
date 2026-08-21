5855baccdb523185c79931db3ea99f18f2ae96c3
Faria Islam Lubna <farialubna1208@gmail.com>
2026-08-21 22:35:07 +0600

    Initial ASP.NET project setup

---
+++ MindCare/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MindCare.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
